d8c9d2d74c3e01460393638de32fb60386f66e2e
ruthviksagi <ruthvik.1202@gmail.com>
2025-04-20 10:19:23 +0100

    Updates

---
+++ precache-manifest.42275187d9862c574232d8f1ce51a02e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bbff04c81438a35e7a9eb4b365e9708",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e83c80775bd25eed165",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "2630568fe5dd7e9791d8",
+    "url": "/static/js/2.a6c81c02.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/static/js/2.a6c81c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfbf512de136ab4d3d82",
+    "url": "/static/js/3.54f2bb70.chunk.js"
+  },
+  {
+    "revision": "0e83c80775bd25eed165",
+    "url": "/static/js/main.0adcd622.chunk.js"
+  },
+  {
+    "revision": "22283677d9fb210699df",
+    "url": "/static/js/runtime-main.8156deeb.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
